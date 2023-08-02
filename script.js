1e871881862b9bff0ca5a005a1ab5ab5dd66df79
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-08-02 11:46:05 +0100

    Created computer choice function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 
-getComputerChoice(){
-    return("rock"||"paper"||"scissors");}
-    ;
+function getComputerChoice(){
+    let randomNo = Math.floor(Math.random() *3);
+    switch(randomNo){
+        case 0:
+            return "rock"
+        case 1:
+            return "scissors"
+        case 2:
+            return "paper"
+    }
+}
 
+console.log(getComputerChoice());
